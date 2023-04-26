e9138dffbfc40f9613b2565cacbbc38e31c8bd74
SaidahmadRasulov <said.rasulov.2018@list.ru>
2023-04-27 01:10:04 +0500

    Finish

--- script.js
+++ script.js
@@ -15,11 +15,49 @@ window.addEventListener('scroll', () => {
 });
 
 
-// if("serviceWorker" in navigator) {
-//     window.addEventListener("load", function() {
-//         navigator.serviceWorker
-//             .register('/serviceWorker.js')
-//             .then(res => console.log('service work register'))
-//             .catch(err => console.log('error', err))
-//     })
-// }
+const form = document.querySelector('#signForm');
+const output = document.getElementById('output_text');
+const registerForm = document.getElementById('form');
+
+function handleSign() {
+    if(form[0].value !== '' && form[1].value !== '') {
+        output.innerHTML = "<h1 class='success_title'>Your account is sign in successfuly!</h1>"
+        setTimeout(() => {
+            output.innerHTML = ''
+        }, 3000)
+        form[0].value = '';
+        form[1].value = '';
+    } else {
+        output.innerHTML = "<h1 class='fail_title'>Your account isn't sign in successfuly!</h1>"
+        setTimeout(() => {
+            output.innerHTML = ''
+        }, 3000)
+    }
+}
+
+console.log(registerForm)
+function handleRegister() {
+    if(registerForm[0].value !== '' && registerForm[1].value !== '' && registerForm[2].value !== '') {
+        output.innerHTML = "<h1 class='success_title'>Your account is register successfuly!</h1>"
+        setTimeout(() => {
+            output.innerHTML = ''
+        }, 3000)
+        registerForm[0].value = '';
+        registerForm[1].value = '';
+        registerForm[2].value = '';
+    } else {
+        output.innerHTML = "<h1 class='fail_title'>Your account isn't register successfuly!</h1>"
+        setTimeout(() => {
+            output.innerHTML = ''
+        }, 3000)
+    }
+}
+
+if("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register('/serviceWorker.js')
+            .then(res => console.log('service work register'))
+            .catch(err => console.log('error', err))
+    })
+}
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,45 +1,43 @@
-// const brex = "dev-coffee-site-v1";
-// const assets = [
-//     "/",
-//     '/index.html',
-//     '/script.js',
-//     '/CSS/style.css',
-//     '/Assets/carouselFirst.png',
-//     '/Assets/carouselSecond.png',
-//     '/Assets/carouselThird.png',
-//     '/Assets/carouselFour.png',
-//     '/Assets/carouselFive.png',
-//     '/Assets/carouselSix.png',
-//     '/Assets/carouselSeven.png',
-//     '/Assets/carouselEight.png',
-//     './Assets/enterprice.png',
-//     './Assets/enterTel.png',
-//     './Assets/homeImg.png',
-//     './Assets/Logo.png',
-//     './Assets/startUp.png',
-//     './Assets/startups.png'
-// ]
+const brex = "dev-coffee-site-v1";
+const assets = [
+    "/",
+    '/index.html',
+    '/script.js',
+    '/CSS/style.css',
+    '/Assets/carouselFirst.png',
+    '/Assets/carouselSecond.png',
+    '/Assets/carouselThird.png',
+    '/Assets/carouselFour.png',
+    '/Assets/carouselFive.png',
+    '/Assets/carouselSix.png',
+    '/Assets/carouselSeven.png',
+    '/Assets/carouselEight.png',
+    './Assets/enterprice.png',
+    './Assets/enterTel.png',
+    './Assets/homeImg.png',
+    './Assets/Logo.png',
+    './Assets/startUp.png',
+    './Assets/startups.png',
+    './Assets/Frame (1).png',
+    './Assets/Frame (2).png',
+    './Assets/Frame (3).png',
+    './Assets/Frame (4).png',
+    './Assets/one.png',
+    './Assets/scl.png',
+]
 
-// self.addEventListener("install", installEvent => {
-//   installEvent.waitUntil(
-//     caches.open(brex).then(cache => {
-//       cache.addAll(assets);
-//     })
-//   )
-// });
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(brex).then(cache => {
+      cache.addAll(assets);
+    })
+  )
+});
 
-// self.addEventListener("fetch", fetchEvent => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then(res => {
-//       return res || fetch(fetchEvent.request)
-//     })
-//   )
-// })
-
-// // self.addEventListener('fetch', fetchEvent => {
-// //     fetchEvent.respondWith(
-// //         caches.match(fetchEvent.request).then(res => {
-// //             return res || fetch(fetchEvent.request)
-// //         })
-// //     )
-// // })
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
